c7328b1ae49cfe2657e5d4f22e395d9b82be75e8
Arun <arun.k.thomas@gmail.com>
2020-06-02 14:22:39 +0530

    my first commit

---
+++ test/test-pages.js
@@ -0,0 +1,31 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+describe('Status and content', function() {
+    describe ('Main page', function() {
+		it('Main page content', function(done) {
+			request('http://localhost:8210' , function(error, response, body) {
+				expect(body).to.equal('Hello World');
+				done();
+			});
+		});
+
+		it('Main page status', function(done) {
+			request('http://localhost:8210' , function(error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			});
+		});
+	});
+	
+	describe ('About page', function() {
+
+		it('About page content', function(done) {
+			request('http://localhost:8210/about' , function(error, response, body) {
+				expect(response.statusCode).to.equal(404);
+				done();
+			});
+		});
+
+	});
+});
